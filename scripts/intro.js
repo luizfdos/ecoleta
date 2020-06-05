af1d49b71dd7c8c6b16d8be387a29a86d018e162
Luizfdos <luizfernando.os@outlook.com>
2020-06-05 09:47:50 -0300

    Segundo dia nlw

---
+++ scripts/intro.js
@@ -0,0 +1,64 @@
+// comentários
+// document.write("Hello")
+// let myvar
+// myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// "Isso é uma string"
+// 'Isso também é uma string'
+// `Isso é uma string também`
+
+// number
+// const n1 = 1 
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande lógica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Mame"} ]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código 
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// sayMyName("Douglas")
+
+
+// const notaFinal = 4
+
+// if( notaFinal < 5 ) {
+//   document.write('Reprovado')
+// } else {
+//   document.write('Aprovado')
+// }
+
+// loop repetições
+
+// for (i = 0; i < 10; i++) {
+//     document.write(`<p>${i}</p>`)
+//   }
+  
+  
+  
